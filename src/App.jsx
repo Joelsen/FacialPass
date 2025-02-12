c02c0ec7b528044398b690003630e04bb65d3369
Joelsen <155982256+Joelsen@users.noreply.github.com>
2025-02-12 16:52:04 +0100

    added student dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import HomePage from './pages/HomePage';
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
-import { AuthProvider, useAuth } from './components/Auth';
-import SignUp from './pages/SignUp';
-import Onboarding from './pages/Onboarding';
-import ParticlesReact from './pages/ParticlesReact';
+import { AuthProvider, useAuth } from "./components/Auth";
+import SignUp from "./pages/SignUp";
+import Onboarding from "./pages/Onboarding";
+import ParticlesReact from "./pages/ParticlesReact";
 import "./App.css";
+import { Student } from "./components/Student";
 
 const ProtectedRoute = ({ children }) => {
   const { isAuthenticated } = useAuth();
@@ -18,7 +24,7 @@ const App = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   const toggleSidebar = () => {
-    setSidebarOpen(prev => !prev);
+    setSidebarOpen((prev) => !prev);
   };
 
   // Method to check if the screen is mobile
@@ -28,10 +34,10 @@ const App = () => {
 
   useEffect(() => {
     checkIfMobile();
-    window.addEventListener('resize', checkIfMobile);
+    window.addEventListener("resize", checkIfMobile);
 
     return () => {
-      window.removeEventListener('resize', checkIfMobile);
+      window.removeEventListener("resize", checkIfMobile);
     };
   }, []);
 
@@ -39,19 +45,42 @@ const App = () => {
     <AuthProvider>
       <Router>
         <Routes>
-          <Route path='/' element={<ParticlesReact />} />
-          <Route path="/login" element={<Login isMobile={isMobile} />} />
-          <Route path="/dashboard" element={
-            // <ProtectedRoute>
-            <HomePage sidebarOpen={sidebarOpen} toggleSidebar={toggleSidebar} isMobile={isMobile} />
-            // </ProtectedRoute>
-          } />
-          <Route path="/signup" element={
-            <SignUp sidebarOpen={sidebarOpen} toggleSidebar={toggleSidebar} isMobile={isMobile} />
-          } />
-          <Route path="/onboarding" element={
-            <Onboarding sidebarOpen={sidebarOpen} toggleSidebar={toggleSidebar} isMobile={isMobile} />
-          } />
+          <Route path="/" element={<ParticlesReact />} />
+          <Route path="/login" element={<Student isMobile={isMobile} />} />
+          {/* <Route path="/student" element={<Student />} /> */}
+
+          <Route
+            path="/dashboard"
+            element={
+              // <ProtectedRoute>
+              <HomePage
+                sidebarOpen={sidebarOpen}
+                toggleSidebar={toggleSidebar}
+                isMobile={isMobile}
+              />
+              // </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/signup"
+            element={
+              <SignUp
+                sidebarOpen={sidebarOpen}
+                toggleSidebar={toggleSidebar}
+                isMobile={isMobile}
+              />
+            }
+          />
+          <Route
+            path="/onboarding"
+            element={
+              <Onboarding
+                sidebarOpen={sidebarOpen}
+                toggleSidebar={toggleSidebar}
+                isMobile={isMobile}
+              />
+            }
+          />
         </Routes>
       </Router>
     </AuthProvider>
